81fd3d606bf1904b3fff57013f7d844137881dae
Merlin de Cloe <codingmerlin@gmail.com>
2022-11-22 18:21:23 +0100

    hoi

--- sources/pipex.c
+++ sources/pipex.c
@@ -6,7 +6,7 @@
 /*   By: mde-cloe <mde-cloe@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/11/14 17:18:56 by mde-cloe      #+#    #+#                 */
-/*   Updated: 2022/11/17 16:05:22 by mde-cloe      ########   odam.nl         */
+/*   Updated: 2022/11/22 18:21:05 by mde-cloe      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,24 @@
 
 int	main(void)
 {
-	int		status;
-	pid_t	pid;
+	int			status;
+	pid_t		pid;
+	char	**argv = {"pwd", "-L"};
 
 	pid = fork();
 	if (pid == 0)
 	{
-		execve("pwd", NULL, NULL);
+		execve("/bin/pwd", argv, NULL);
 		ft_printf("exec failed :(\n");
-		perror("hey\n");
+		perror("error =");
 	}
 
 	waitpid(pid, &status, 0);
 	//status can also be NULL
 	return (0);
 }
+
+
+// for exec we need to split the path and the command name
+
+//usage: pwd [-L | -P]
